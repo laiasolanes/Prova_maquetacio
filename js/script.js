fb1340f0b0368067e8f748dfe02c239534d3f84b
Laia Solanes <laiasolanes@gmail.com>
2021-04-11 11:30:56 +0200

    Create, styled and add functionality blog section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /**
-  * @desc add a class active to nav buttons
+  * @desc Active class to nav buttons
 */
 let buttons = document.querySelectorAll('.button');
 console.log(buttons);
@@ -12,7 +12,7 @@ buttons.forEach(button => {
 
 
 /**
-  * @desc accordion buttons in Why Choose us column
+  * @desc Accordion buttons in Why Choose us column
 */
 let accordion = document.querySelectorAll(".accordion");
 let i;
@@ -31,3 +31,55 @@ for (i = 0; i < accordion.length; i++) {
     } 
   });
 }
+
+
+/**
+  * @desc Blog slider
+*/
+
+const slide = {};
+const cardsSlider = (function() {
+  
+let slides    = document.querySelectorAll('.cards__item'),
+    NEXT      = document.querySelector('.cards__controls--next'),
+    PREVIOUS  = document.querySelector('.cards__controls--prev'),
+    count     = 0;
+  
+  NEXT.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    if(count < (slides.length - 1)) {
+      count++;
+      
+      for(let i = 0; i < slides.length; i++) {
+        let element = slides[i],
+            trans   = 20 + element.clientWidth;
+        
+        slide.transform = trans * count;
+        
+        element.style.transform = 'translateX(-' + slide.transform + 'px)'
+        
+        console.log(slide.transform)
+      }
+    }
+  })
+  
+  
+  PREVIOUS.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    if(count > 0) {
+      count--;
+      
+      for (let i = 0; i < slides.length; i++) {
+        let element = slides[i],
+            trans   = 20 + element.clientWidth;
+        
+        slide.transform = trans * count;
+                
+        element.style.transform = 'translateX(-' + slide.transform + 'px)'        
+      }
+    }
+  })
+  
+})();
